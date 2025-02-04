6b313695a726a2af5c8ca3b96d51ba27a7ac804a
Matheus Cristian <81980814+matheuscristian@users.noreply.github.com>
2025-02-04 08:39:16 -0300

    Create a.cs

---
+++ a.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace PATrilhasAtiv05
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			DateTime date = DateTime.Now;
+			
+			Console.WriteLine(date.ToLocalTime());
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
